adb0880f1a9bdbc769e8796bb2f8a3ee37e0864c
fairyspace <krhudsong@gmail.com>
2026-06-28 23:22:05 +0800

    项目架子

---
+++ src/main/java/com/fairyspace/fanfanmagic/FanfanMagicApplication.java
@@ -0,0 +1,13 @@
+package com.fairyspace.fanfanmagic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanfanMagicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FanfanMagicApplication.class, args);
+    }
+
+}
